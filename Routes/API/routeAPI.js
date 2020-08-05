ae5f027ff7de8598d5b776d1b2f87ec61a13e32e
DarkDave <darkdave1185@gmail.com>
2020-08-05 16:13:40 -0700

    got note taker to work as requested but getting error, try to resolve

--- Routes/API/routeAPI.js
+++ Routes/API/routeAPI.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const fs = require(`fs`);
 
-console.log(db)
-    //POST ROUTE//
-    //The following API routes should be created:
+const idFilter = req => member => member.id === parseInt(req.params.id);
+
+//POST ROUTE//
+//The following API routes should be created:
 
 //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 router.get("/api/notes", (request, response) => {
@@ -14,20 +15,20 @@ router.get("/api/notes", (request, response) => {
 });
 
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-router.post("/api/notes", function(request, response) {
+router.post("/api/notes", (request, response) => {
     //  const postNote = fs.readFileSync(`../../db/db.json`);
-    var id = 1;
+    let id = 1;
     if (db.length < 0) {
         id = db[db.length - 1].id + 1;
     }
-    var newNote = {
+    let newNote = {
         id: id,
         title: request.body.title,
         text: request.body.text
     };
     db.push(newNote);
 
-    fs.writeFile("../../db/db.json", JSON.stringify(db), function(err) {
+    fs.writeFile("../../db/db.json", JSON.stringify(db), (err) => {
         response.json(db);
     })
 });
@@ -36,6 +37,11 @@ router.post("/api/notes", function(request, response) {
 // req.body is available since we're using the body parsing middleware
 
 //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-router.post("/api/clear", (c, d) => {});
+router.delete("/api/notes/:id", (request, response) => {
+    db.splice(request.params.id);
+    response.json(db);
+    response.send({ type: `DELETE` });
+    console.log(request.params.id);
+});
 
 module.exports = router;
--- Routes/HTML/routeHTML.js
+++ Routes/HTML/routeHTML.js
@@ -5,12 +5,12 @@ const path = require(`path`);
 //GET ROUTE//
 //The following HTML routes should be created:
 //GET /notes - Should return the notes.html file.
-router.get("/notes", (c, d) => {
-    d.sendFile(path.join(__dirname, "../../public/notes.html"));
+router.get("/notes", (request, response) => {
+    response.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 //GET * - Should return the index.html file
-router.get("/", (c, d) => {
-    d.sendFile(path.join(__dirname, "../../public/index.html"));
+router.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
